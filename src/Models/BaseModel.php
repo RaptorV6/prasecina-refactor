dce64b0e7ffc07d8ed456dd2d7ffe326c2dfc5a4
RaptorV6 <142246142+RaptorV6@users.noreply.github.com>
2024-04-18 14:21:36 +0200

    Move BaseModel to src/Models with namespace

---
+++ src/Models/BaseModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Refactor\Models;
+
+class BaseModel {
+    protected $db;
+    public function __construct($dbConn) {
+        $this->db = $dbConn;
+    }
+    public function query($sql) {
+        return $this->db->query($sql);
+    }
+    public function fetchAll($sql) {
+        return $this->db->query($sql)->fetch_all(PAO_FETCH_ASSOC);
+    }
+}
+
+?>
